3b28ef0ca496d94749fa509a13841c5bc379653e
orgescicocit <orges.cico@cit.edu.al>
2017-06-19 13:14:51 +0200

    first time upload project
    this is the first implementation

---
+++ VendingMachineArrays-0/VendingMachineArrays/it/polito/vm/Beverage.java
@@ -0,0 +1,21 @@
+package it.polito.vm;
+
+class Beverage {
+
+	private String name;
+	private double price;
+
+	public Beverage(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+	
+}
---
+++ VendingMachineArrays-0/VendingMachineArrays/it/polito/vm/Column.java
@@ -0,0 +1,24 @@
+package it.polito.vm;
+
+class Column {
+
+	private String name;
+	private int quantity;
+
+	public Column(String name, int quantity) {
+		this.name = name;
+		this.quantity = quantity;
+	}
+
+	public String getNome() {
+		return name;
+	}
+
+	public void decrementQuantity() {
+		quantity--;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+}
